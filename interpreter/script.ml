568cfb11e1de6052367b19dd967fcefde85b0c4e
dwclake <clake.dw@gmail.com>
2023-07-12 14:54:05 -0700

    Adjusted script parsing

--- interpreter/script.ml
+++ interpreter/script.ml
@@ -1,18 +1,8 @@
 open Core
 
 let run_script file =
-    let input = 
+    let program = 
         Stdio.In_channel.read_all file
-        |> String.split ~on:'\n' 
-    in
-    let program = 
-        List.fold 
-            input 
-            ~init:"" 
-            ~f:(fun x accum -> x ^ accum)
-    in
-    let program = 
-        program
         |> new Lexer.t |> ref
         |> Parser.create
         |> Parser.parse_program
